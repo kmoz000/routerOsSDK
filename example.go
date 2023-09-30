15a4daf6de79f721bd6adafd7358df6b1ae29aa9
Niceblueman <abdelkarim@ispapp.co>
2023-09-30 18:05:39 +0100

    docs

---
+++ example.go
@@ -0,0 +1,32 @@
+// routerOs/routerOsDebugger/cmd.go
+package routerOsSDK
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	RouterOsSDK "github.com/kmoz000/routerOsSDK"
+	"github.com/spf13/cobra"
+)
+var (
+    username   string =  "admin"
+    password   string = ""
+    address    string = "http://192.168.1.1/rest"
+)
+func example(){
+	c, err := RouterOsSDK.NewCient(address, RouterOsSDK.WithRequestEditorFn(func(ctx context.Context, req *http.Request) error {
+		req.SetBasicAuth(username, password)
+		return nil
+	}))
+	if err != nil {
+		fmt.Printf("Error Debugging RouterOS device at %s\nError> \t %v", address, err.Error())
+		return
+	}
+	if res.StatusCode() != 200 {
+		fmt.Printf("Error Debugging RouterOS device at %s\nStatusCode> \t", address, res.StatusCode())
+		return
+	}
+	fmt.Printf("results: %v", res)
+}
